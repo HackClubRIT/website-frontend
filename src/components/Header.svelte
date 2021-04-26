e4bb0c483a5a6f104d8abf500e00760c1174a218
irenekurien <irenekurien01@gmail.com>
2021-04-27 00:29:41 +0530

    small fix

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,7 +5,7 @@
 
 <div class="w-full py-16 bg-dark text-white">
   <div class="w-3/4 md:w-2/3 mx-auto top-0 md:px-10 text-center">
-    <h1 class="text-4xl md:text-6xl">{title}</h1>
+    <h1 class="text-2xl md:text-6xl">{title}</h1>
     <p class="text-xl w-2/3 mx-auto">
       {description}
     </p>
